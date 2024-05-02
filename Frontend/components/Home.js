ea334bd629cd4f0fae9517cf48ddd53d8d3c67bb
David.Kfld <david.s.kufeld@gmail.com>
2024-05-02 15:01:04 +0200

    1er commit

--- Frontend/components/Home.js
+++ Frontend/components/Home.js
@@ -1,14 +1,31 @@
 import styles from '../styles/Home.module.css';
+import Image from 'next/image'
 
 function Home() {
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+
+    <div className={styles.main}>
+      <div className={styles.leftHome}>
+        <Image className={styles.logo} src='/logo_twitter.webp'
+          height={200}
+          width={300} />
+      </div>
+      <div className={styles.rightHome} >
+        <h1>See what's happening.</h1>
+        <h3>Join Hackatweet today</h3>
+        <button type='button' >Sign up</button>
+        <p>Already have an account ?</p>
+        <button type='button'> Sign in</button>
+
+
+
+
+      </div>
+
     </div>
+
+
+
   );
 }
 
