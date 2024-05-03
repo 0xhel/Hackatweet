966f55cb0bf681df8ce4a9b8a87941ffdf76c81f
Sterben <0xsterben@tutamail.com>
2024-05-03 13:01:05 +0200

    toto

--- Backend/routes/tweets.js
+++ Backend/routes/tweets.js
@@ -5,9 +5,21 @@ require('../models/connection');
 const Tweet = require('../models/tweets');
 const { checkBody } = require("../modules/checkBody");
 
-router.post('/tweet', (req, res) => {
-    if (!checkBody(req.body, ['tweet'])) {
-        res.json({ result: false, error: 'Missing or empty fields' });
-        return;
+router.post("/tweets", (req, res) => {
+    if (!checkBody(req.body, ['tweetContent'])) {
+        return res.json({ result: false, error: 'Missing or empty fields' });
     }
-})
+
+    const newTweet = new Tweet({
+        tweetContent: req.body.tweetContent
+    });
+
+    newTweet.save((savedTweet) => {
+        if (!savedTweet) {
+            return res.json({ result: false, error: 'Error saving tweet' });
+        }
+        return res.json({ result: true, tweet: savedTweet });
+    });
+});
+
+module.exports = router;
